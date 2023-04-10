e58c8e95a973976132c2bcd6954462d7f520f0b3
duyvn <duyvn@DAHLIA3003>
2023-04-10 18:52:50 +0700

    Add connection class

---
+++ DBMS_FINAL_PROJECT/_ConnectSQL/myDB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV_Project
+{
+    public partial class MyDB
+    {
+        SqlConnection con = new SqlConnection(@"");
+
+        // get the connection
+        public SqlConnection getConnection
+        {
+            get
+            {
+                return con;
+            }
+        }
+
+
+        // open the connection
+        public void openConnection()
+        {
+            if ((con.State == ConnectionState.Closed))
+            {
+                con.Open();
+            }
+
+        }
+
+
+        // close the connection
+        public void closeConnection()
+        {
+            if ((con.State == ConnectionState.Open))
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
